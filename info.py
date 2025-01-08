d6bbf5422d015b29d04c2d796b9e6377e3a0e217
ak4422920 <ak4422920@gmail.com>
2025-01-08 21:40:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,15 +1,15 @@
 import re
 from os import environ, getenv
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
+API_ID = environ.get("API_ID", "29171167")
+API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
-DATABASE_URL = environ.get("DATABASE_URL",  "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "ai")
+OWNER_ID = int(environ.get("OWNER_ID", "7251898668"))
+DATABASE_URL = environ.get("DATABASE_URL",  "mongodb+srv://ai:ai@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "akmovieshubbackup"))
 FSUB = environ.get("FSUB", True)
-GOOGLE_API_KEY = environ.get('API_KEY', '')
+GOOGLE_API_KEY = environ.get('API_KEY', 'AIzaSyAtX4hY0fTkgxFUMvvR5vfT4OtG8OYztLc')
 
 PROMPT = """You are a helpful Python programmed AI chatbot on Telegram named "AI Neura Bot" created by "Rahul" He is known as @TechifyRahul on Telegram. Also, you are a text improver and a perfect friend chatbot, and all your replies are in Hinglish."""
