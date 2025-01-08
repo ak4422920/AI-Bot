feea1989d89b7097d54abe32a158e83b77602104
ak4422920 <ak4422920@gmail.com>
2025-01-08 22:03:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -8,7 +8,7 @@ OWNER_ID = int(environ.get("OWNER_ID", "7251898668"))
 DATABASE_URL = environ.get("DATABASE_URL",  "mongodb+srv://ai:ai@cluster0.fgu4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002338054057"))
-AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "akmovieshubbackup"))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1001785093771"))
 FSUB = environ.get("FSUB", True)
 GOOGLE_API_KEY = environ.get('API_KEY', '')
 
